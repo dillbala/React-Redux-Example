ba5a6b2da7ef3c50e7526e7b3a0ef79a0d3fa8d2
dillbala <mailtobalamurugan3@gmail.com>
2017-07-13 19:18:32 +0530

    calling youtube api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import React , {Component} from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import Home from './component/home'
+const API_KEY = 'AIzaSyAiKnQ00il8v4dmMDpe9qC6m-ks0AAIdzU';
+
 
 
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            videos : [],
+            selectedVideo : null
+        }
+        
+        YTSearch({key:API_KEY,term : 'react tutorial'},(videos) => {
+            //this.setState({videos});
+            this.setState({
+                videos : videos,
+                selectedVideo : videos[0]
+            });
+        });
+    }
     render() {
         return (
             <div>
-                <h1>Hello World!</h1>
-                <Home />
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                onVideoSelect = {(selectedVideo)=>this.setState({selectedVideo})}
+                videos= {this.state.videos} />
             </div>
         );
     }
